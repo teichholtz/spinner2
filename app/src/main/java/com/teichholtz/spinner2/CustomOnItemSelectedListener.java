63e8242a16a407c50ca18dcbc2f6e763404c91d3
teichholtz <davet@teichholtz.com>
2016-05-15 20:09:12 -0400

    Add a text field to display the selection.  Toast display the position and the value of the selection.

--- app/src/main/java/com/teichholtz/spinner2/CustomOnItemSelectedListener.java
+++ app/src/main/java/com/teichholtz/spinner2/CustomOnItemSelectedListener.java
@@ -1,9 +1,22 @@
 package com.teichholtz.spinner2;
 
 // probably want to tie the text box in.
-import android.view.View;
+
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.TextView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class CustomOnItemSelectedListener implements OnItemSelectedListener {
@@ -12,6 +25,7 @@ public class CustomOnItemSelectedListener implements OnItemSelectedListener {
         Toast.makeText(parent.getContext(),
                 "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
                 Toast.LENGTH_SHORT).show();
+        //TextView t = (TextView)findViewById(R.id.textView);
     }
 
     @Override
--- app/src/main/java/com/teichholtz/spinner2/MainActivity.java
+++ app/src/main/java/com/teichholtz/spinner2/MainActivity.java
@@ -11,11 +11,13 @@ import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Spinner spinner1, spinner2;
     private Button btnSubmit;
+    public  TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     public void addListenerOnButton() {
 
         spinner1 = (Spinner) findViewById(R.id.spinner1);
+        t = (TextView)findViewById(R.id.textView);
+        t.setText("some static text");
 
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
@@ -49,6 +53,23 @@ public class MainActivity extends AppCompatActivity {
                         "OnClickListener : " +
                                 "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()),
                         Toast.LENGTH_SHORT).show();
+                // get the value of what was selected
+                String Selected;
+                int position = spinner1.getSelectedItemPosition();
+                String combined;
+                combined = "Position " + Integer.toString(position);
+                Toast.makeText(MainActivity.this,combined,Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,Integer.toString(position),Toast.LENGTH_SHORT).show();
+
+                Selected = String.valueOf(spinner1.getSelectedItem());
+
+                if (Selected.equals("Italy"))
+                        t.setText("I was there");
+                else
+                        t.setText("Nope, not Italy");
+
+                //t.setText(Selected);
+
             }
 
         });
