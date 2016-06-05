09b3043c4d914fa5f0f647034b59e4fe0a8cf84c
teichholtz <davet@teichholtz.com>
2016-06-05 19:21:03 -0400

    Added in conference rooms.  Added in a routine to return the room via index in array. Changed app label to 'Conference'

--- app/src/main/java/com/teichholtz/spinner2/MainActivity.java
+++ app/src/main/java/com/teichholtz/spinner2/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         spinner1 = (Spinner) findViewById(R.id.spinner1);
         t = (TextView)findViewById(R.id.textView);
-        t.setText("some static text");
+        t.setText("Pick a room, any room");
 
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
@@ -49,24 +49,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(MainActivity.this,
-                        "OnClickListener : " +
-                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()),
-                        Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this,
+                //        "OnClickListener : " +
+                //                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()),
+                //        Toast.LENGTH_SHORT).show();
                 // get the value of what was selected
                 String Selected;
                 int position = spinner1.getSelectedItemPosition();
                 String combined;
                 combined = "Position " + Integer.toString(position);
-                Toast.makeText(MainActivity.this,combined,Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this,Integer.toString(position),Toast.LENGTH_SHORT).show();
-
+                //Toast.makeText(MainActivity.this,combined,Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this,Integer.toString(position),Toast.LENGTH_SHORT).show();
+                combined=GetLocation(position);
                 Selected = String.valueOf(spinner1.getSelectedItem());
 
-                if (Selected.equals("Italy"))
-                        t.setText("I was there");
-                else
-                        t.setText("Nope, not Italy");
+                t.setText(combined);
+
+                //if (Selected.equals("Italy"))
+                //        t.setText("I was there");
+                //else
+                //        t.setText("Nope, not Italy");
 
                 //t.setText(Selected);
 
@@ -75,6 +77,78 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public java.lang.String GetLocation(int position) {
+        String combined;
+        combined = "Position from routine " + Integer.toString(position);
+        //Toast.makeText(MainActivity.this,combined,Toast.LENGTH_SHORT).show();
+        //Toast.makeText(MainActivity.this,Integer.toString(position),Toast.LENGTH_SHORT).show();
 
 
+        // declares an array of strings
+        String[] confLocArray;
+
+        // allocates memory for 29 strings (we have that many conference rooms!!)
+        confLocArray = new String[30];
+
+        // initialize the elements
+        //Copley
+	confLocArray[0] = "First floor near Event Production" ;
+	//Boylston
+        confLocArray[1] = "First floor near Event Production";
+	//Atlantic
+        confLocArray[2] = "Second floor, Tech area";
+	//Sunset
+        confLocArray[3] = "Second floor, small room in tech area";
+	// Rodeo
+        confLocArray[4] = "Second floor, near Corp IT";
+	// Bedford
+        confLocArray[5] = "Second floor, small room next to Corp IT";
+	// Bourbon
+        confLocArray[6] = "Second floor near little steps, Window looks out on Channel Center";
+	// The Library
+        confLocArray[7] = "Second floor, middle of office,Window looks out on A street";
+	// Broadway
+        confLocArray[8] = "Second floor, by Cafe area, windows looks out to Channel Center";
+	// Pike
+        confLocArray[9] = "Second floor, small room by Cafe area, ";
+	// Market
+        confLocArray[10] = "Second floor, by Cafe area, windows look out on A street";
+	    // Filmore
+        confLocArray[11] = "Second floor in the annex";
+	// Tremont 
+        confLocArray[12] = "Third floor, hidden away in the corner by AP/AR";
+	// Capri
+        confLocArray[13] = "Third floor Mothers room";
+	// Beale
+        confLocArray[14] = "Third floor, small room by Talent";
+	// Lombard
+        confLocArray[15] = "Third floor, small room by Talent";
+	// Bleeker
+        confLocArray[16] = "Third floor, small room by Talent";
+	// Oak
+        confLocArray[17] = "Third floor, big room by Talent";
+	// Spring
+        confLocArray[18] = "Third floor, small room by Talent (next to Oak)";
+	// Newbury
+        confLocArray[19] = "Third floor, by reception";
+	// Peachtree
+        confLocArray[20] = "Third floor, by reception";
+	// Mulholland
+        confLocArray[22] = "Third floor, by cafe, windows look out on A street";
+	// Ocean
+        confLocArray[23] = "Third floor, by cafe, small room next to Mulholland";
+	// Madison
+        confLocArray[24] = "Third floor, by cafe, windows look out on Channel Center";
+	//Michigan
+        confLocArray[25] = "Third Floor, annex, windows look out on A street";
+	// Greenwich
+        confLocArray[26] = "Third floor, annex, small room, windows look out on A street";
+	// LakeShore
+        confLocArray[27] = "Third floor annex";
+	// Charles
+        confLocArray[28] = "Third floor annex";
+        confLocArray[29] = "Not assigned.";
+	
+	return confLocArray[position];
+}
 }
