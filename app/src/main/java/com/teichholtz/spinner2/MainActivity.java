1c8553f8dcf897872fc5ccc25f70d9f596f7f0a2
teichholtz <davet@teichholtz.com>
2016-06-10 21:51:50 -0400

    Put room names in alphabetical order

--- app/src/main/java/com/teichholtz/spinner2/MainActivity.java
+++ app/src/main/java/com/teichholtz/spinner2/MainActivity.java
@@ -92,61 +92,61 @@ public class MainActivity extends AppCompatActivity {
 
         // initialize the elements
         //Copley
-	confLocArray[0] = "First floor near Event Production" ;
+	confLocArray[9] = "First floor near Event Production" ;
 	//Boylston
-        confLocArray[1] = "First floor near Event Production";
+        confLocArray[5] = "First floor near Event Production";
 	//Atlantic
-        confLocArray[2] = "Second floor, Tech area";
+        confLocArray[0] = "Second floor, Tech area";
 	//Sunset
-        confLocArray[3] = "Second floor, small room in tech area";
+        confLocArray[25] = "Second floor, small room in tech area";
 	// Rodeo
-        confLocArray[4] = "Second floor, near Corp IT";
+        confLocArray[23] = "Second floor, near Corp IT";
 	// Bedford
-        confLocArray[5] = "Second floor, small room next to Corp IT";
+        confLocArray[2] = "Second floor, small room next to Corp IT";
 	// Bourbon
-        confLocArray[6] = "Second floor near little steps, Window looks out on Channel Center";
+        confLocArray[4] = "Second floor near little steps, Window looks out on Channel Center";
 	// The Library
-        confLocArray[7] = "Second floor, middle of office,Window looks out on A street";
+        confLocArray[26] = "Second floor, middle of office,Window looks out on A street";
 	// Broadway
-        confLocArray[8] = "Second floor, by Cafe area, windows looks out to Channel Center";
+        confLocArray[6] = "Second floor, by Cafe area, windows looks out to Channel Center";
 	// Pike
-        confLocArray[9] = "Second floor, small room by Cafe area, ";
+        confLocArray[22] = "Second floor, small room by Cafe area, ";
 	// Market
-        confLocArray[10] = "Second floor, by Cafe area, windows look out on A street";
+        confLocArray[15] = "Second floor, by Cafe area, windows look out on A street";
 	    // Filmore
-        confLocArray[11] = "Second floor in the annex";
+        confLocArray[10] = "Second floor in the annex";
 	// Tremont 
-        confLocArray[12] = "Third floor, hidden away in the corner by AP/AR";
+        confLocArray[27] = "Third floor, hidden away in the corner by AP/AR";
 	// Capri
-        confLocArray[13] = "Third floor Mothers room";
+        confLocArray[7] = "Third floor Mothers room";
 	// Beale
-        confLocArray[14] = "Third floor, small room by Talent";
+        confLocArray[1] = "Third floor, small room by Talent";
 	// Lombard
-        confLocArray[15] = "Third floor, small room by Talent";
+        confLocArray[13] = "Third floor, small room by Talent";
 	// Bleeker
-        confLocArray[16] = "Third floor, small room by Talent";
+        confLocArray[3] = "Third floor, small room by Talent";
 	// Oak
-        confLocArray[17] = "Third floor, big room by Talent";
+        confLocArray[19] = "Third floor, big room by Talent";
 	// Spring
-        confLocArray[18] = "Third floor, small room by Talent (next to Oak)";
+        confLocArray[24] = "Third floor, small room by Talent (next to Oak)";
 	// Newbury
-        confLocArray[19] = "Third floor, by reception";
+        confLocArray[18] = "Third floor, by reception";
 	// Peachtree
-        confLocArray[20] = "Third floor, by reception";
+        confLocArray[21] = "Third floor, by reception";
 	// Mulholland
-        confLocArray[22] = "Third floor, by cafe, windows look out on A street";
+        confLocArray[17] = "Third floor, by cafe, windows look out on A street";
 	// Ocean
-        confLocArray[23] = "Third floor, by cafe, small room next to Mulholland";
+        confLocArray[20] = "Third floor, by cafe, small room next to Mulholland";
 	// Madison
-        confLocArray[24] = "Third floor, by cafe, windows look out on Channel Center";
+        confLocArray[14] = "Third floor, by cafe, windows look out on Channel Center";
 	//Michigan
-        confLocArray[25] = "Third Floor, annex, windows look out on A street";
+        confLocArray[16] = "Third Floor, annex, windows look out on A street";
 	// Greenwich
-        confLocArray[26] = "Third floor, annex, small room, windows look out on A street";
+        confLocArray[11] = "Third floor, annex, small room, windows look out on A street";
 	// LakeShore
-        confLocArray[27] = "Third floor annex";
+        confLocArray[12] = "Third floor annex";
 	// Charles
-        confLocArray[28] = "Third floor annex";
+        confLocArray[8] = "Third floor annex";
         confLocArray[29] = "Not assigned.";
 	
 	return confLocArray[position];
